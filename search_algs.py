869a91f9f18db0f6d9978f49f0f7af6e6c0324cf
Mateo Rincon <mateorinpin@gmail.com>
2024-07-03 22:47:30 -0500

    adding goal_test arg to dfs and returning search order

--- search_algs.py
+++ search_algs.py
@@ -21,17 +21,20 @@ def dfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], Li
     frontier: Stack[T] = Stack()
     frontier.push(initial)
     explored: Set[T] = {initial}
+    order: List[T] = []
 
     while not frontier.empty:
         current : T = frontier.pop()
+        order.append(current)
 
-        if goal_test(current):
-            return current
+        if goal_test and goal_test(current): #false if goal_test is None
+            return (current, order)
 
         for child in successors(current):
             if child in explored:
                 continue
-            frontier.push(child)
             explored.add(child)
+            frontier.push(child)
+            
     
-    return None
+    return (None, order)
